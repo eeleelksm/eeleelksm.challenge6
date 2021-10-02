f094ac97bd015d5fb9c8fa86b15e2ad21463d6b9
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-10-02 18:06:01 -0400

    html skeleton and js - stuck

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,49 @@
+var allCities = [];
+var searchFormEl = document.querySelector("#search-form");
+var cityEl = document.querySelector("#city");
+
+// save cities viewed into local storage
+var saveSearch = function() {
+  localStorage.item("allCities", JSON.stringify(allCities));
+}
+
 var getWeather = function(lat, lon) {
   // formatting the weather API
-  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=c9c512d1b8bc842f2acb7dc528d85eb3";
+  var apiKey = "c9c512d1b8bc842f2acb7dc528d85eb3";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=" + apiKey;
 
-  // make a request to the url
-  fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-      console.log(data);
+  fetch(apiUrl)
+  .then(function(response) {
+    response.json.then(function(data) {
+      console.log(data, lat, lon);
     });
   });
 };
 
-// var getWeather = function() {
-//   console.log("hello where's the weather");
+
+
+// var formSubmitHandler = function(event) {
+//   event.preventDefault();
+//   console.log(event);
+//    var city = searchFormEl.value.trim();
+//       if (city) {
+  //      
+//       }
+// }
+
+
+// var getWeather = function(lat, lon) {
+//   // formatting the weather API
+//   var apiKey = "c9c512d1b8bc842f2acb7dc528d85eb3";
+//   var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=" + apiKey;
+
+//   // make a request to the url
+//   fetch(apiUrl)
+//   .then(function(response) {
+//     response.json().then(function(data) {
+//       console.log(data);
+//     });
+//   });
 // };
 
-// getWeather();
+// searchFormEl.addEventListener("submit", formSubmitHandler);
